cf91e356edfcb2cf5e5eba02bc20537c5cd8d5d0
Shannon <shannon@reservamos.com>
2024-11-14 16:56:47 -0600

    create recommendation controller and views

---
+++ app/controllers/recommendations_controller.rb
@@ -0,0 +1,14 @@
+class RecommendationsController < ApplicationController
+  def new
+  end
+
+  def create
+    user_query = params[:user_query]
+    @response = "Recomendación basada en '#{user_query}'"  # Este es un ejemplo
+    render :show
+  end
+
+  def show
+    @response
+  end
+end
